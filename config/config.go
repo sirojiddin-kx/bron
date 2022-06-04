a690bc75f26726c6201b0bf0e682c661d133a75c
Sirojiddin Khomidov <khomidovsirojiddin@gmail.com>
2022-06-04 12:18:27 +0500

    project configuration is intialized

---
+++ config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/cast"
+)
+
+type Config struct {
+	Environment string
+
+	PostgresHost     string
+	PostgrestPort    int
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDatabase string
+
+	LogLevel string
+	HttpPort string
+}
+
+func Load() Config {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env fild found", err)
+	}
+	config := Config{}
+
+	config.Environment = cast.ToString(getOrReturnDefault("ENVIRONMENT", "develop"))
+
+	config.PostgresHost = cast.ToString(getOrReturnDefault("POSTGRES_HOST", "localhost"))
+	config.PostgrestPort = cast.ToInt(getOrReturnDefault("POSTGRES_PORT", 5432))
+	config.PostgresUser = cast.ToString(getOrReturnDefault("POSTGRES_USER", "sirojiddin"))
+	config.PostgresPassword = cast.ToString(getOrReturnDefault("POSTGRES_PASSWORD", "1234"))
+	config.PostgresDatabase = cast.ToString(getOrReturnDefault("POSTGRES_DATABASE", "bron_service"))
+
+	config.HttpPort = cast.ToString(getOrReturnDefault("HTTP_PORT", ":8002"))
+
+	return config
+
+}
+
+func getOrReturnDefault(key string, defaultValue interface{}) interface{} {
+	_, exists := os.LookupEnv(key)
+
+	if exists {
+		return os.Getenv(key)
+	}
+
+	return defaultValue
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sirojiddin-kh/bron
+
+go 1.17
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/spf13/cast v1.5.0
+)
