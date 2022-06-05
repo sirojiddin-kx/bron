f391d7ae5dbb80eb2e3db1a1a85823a279891b8b
Sirojiddin Khomidov <khomidovsirojiddin@gmail.com>
2022-06-05 17:51:26 +0500

    CRUD for company service

---
+++ api/models/company_service.go
@@ -0,0 +1,21 @@
+package models
+
+type CompanyServiceCreate struct {
+	Title     string `json:"title"`
+	Duration  int32  `json:"duration"`
+	Price     int32  `json:"price"`
+	CompanyID string `json:"company_id"`
+}
+
+type CompanyService struct {
+	ID        string `json:"guid"`
+	Title     string `json:"title"`
+	Duration  int32  `json:"duration"`
+	Price     int32  `json:"price"`
+	CompanyID string `json:"company_id"`
+}
+
+type CompanyServices struct {
+	Services []CompanyService `json:"services"`
+	Count    int64            `json:"count" db:"count"`
+}
